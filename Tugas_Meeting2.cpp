df8bb1afe7409f2305a9d55e67981f276f476542
vanyawidya <56577127+vanyawidya@users.noreply.github.com>
2021-03-02 10:58:36 +0700

    Add files via upload

---
+++ Tugas_Meeting2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	int x,y,a,b,c,d;
+	x=40,y=5;
+
+	cout<<"Masukkan Nilai x : "<<x<<endl;
+	cout<<"Masukkan Nilai y : "<<y<<endl;
