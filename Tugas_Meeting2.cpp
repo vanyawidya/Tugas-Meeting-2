79da86e7a147655826329f986755a58482e638f4
vanyawidya <56577127+vanyawidya@users.noreply.github.com>
2021-03-02 11:02:11 +0700

    Update Tugas_Meeting2.cpp
    ini sudah diubah

--- Tugas_Meeting2.cpp
+++ Tugas_Meeting2.cpp
@@ -8,3 +8,18 @@ int main(int argc, char** argv)
 
 	cout<<"Masukkan Nilai x : "<<x<<endl;
 	cout<<"Masukkan Nilai y : "<<y<<endl;
+	
+	a=x+y;
+	b=x-y;
+	c=x*y;
+	d=x/y;
+	
+	Cout<<"Hasil Penjumlahan dari X dan Y adalah : "<<a<<endl;
+	Cout<<"Hasil Pengurangan dari X dan Y adalah : "<<b<<endl;
+	Cout<<"Hasil Perkalian dari X dan Y adalah : "<<c<<endl;
+	Cout<<"Hasil Pembagian dari X dan Y adalah : "<<d<<endl;
+	return 0;
+	
+}
+
+//181402123_VanyaWidyaMeifarizkaS
