cf8fca648ecff40d6c09a913f5e535773791593f
vanyawidya <56577127+vanyawidya@users.noreply.github.com>
2021-03-02 11:15:33 +0700

    Update Tugas_Meeting2.cpp

--- Tugas_Meeting2.cpp
+++ Tugas_Meeting2.cpp
@@ -11,11 +11,13 @@ int main(int argc, char** argv)
 	a=x+y;
 	b=x-y;
 	c=x*y;
+	d=x/y;
 	
 	
 	cout<<"Hasil Penjumlahan dari X dan Y adalah : "<<a<<endl;
 	cout<<"Hasil Pengurangan dari X dan Y adalah : "<<b<<endl;
 	cout<<"Hasil Perkalian dari X dan Y adalah : "<<c<<endl;
+	cout<<"Hasil Pembagian dari X dan Y adalah : "<<d<<endl;
 	
 	return 0;
 	
